45f9915449e5750c27af53b1c696489a288e5651
houseoffuture <31938510+houseoffuture@users.noreply.github.com>
2026-05-21 21:03:27 +0300

    Update index.js
    remove version control

--- index.js
+++ index.js
@@ -5,26 +5,17 @@ const app = require('./app');
     let plugin;
     try {
         const opt = getOptFromArgs();
-        const minversion = '5.19.3';
-        const version = opt.version;
         const pluginapi = opt && opt.pluginapi ? opt.pluginapi : 'ih-plugin-api';
         plugin = require(pluginapi + '/index.js')();
 
-        if (version >= minversion) {
-            plugin.log('Plugin is start.', 0);
+        plugin.log('Plugin is start.', 0);
 
-            plugin.params = await plugin.params.get();
-            plugin.log('Recieve plugin parametrs: ' + util.inspect(plugin.params), 1);
+        plugin.params = await plugin.params.get();
+        plugin.log('Recieve plugin parametrs: ' + util.inspect(plugin.params), 1);
 
-            plugin.logger.setParams({ logsize: plugin.params.logsize, logrotate: plugin.params.logrotate });
+        plugin.logger.setParams({ logsize: plugin.params.logsize, logrotate: plugin.params.logrotate });
 
-            app(plugin);
-        }
-        else {
-            plugin.log("Plugin not start! Update system to version: " + minversion + " || Current version: " + version);
-            plugin.exit(17, `Error: system version!`);
-        }
-
+        app(plugin);
 
     } catch (err) {
         plugin.exit(8, `Error: ${util.inspect(err)}`);
